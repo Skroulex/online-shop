40148265650c50f8bac8756bbea40da558f2e1f0
Skroulex <eshimbekov91@gmail.com>
2022-07-01 14:29:41 +0600

    first commit

---
+++ src/Routing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Route, Routes} from "react-router-dom";
+
+const Routing = () => {
+    return (
+        <Routes>
+            <Route />
+        </Routes>
+    );
+};
+
+export default Routing;
